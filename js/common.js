af9740565f95283ff8f45efaba9ff65b337a346f
whats0n <mrdvv@mail.ru>
2015-02-18 14:23:26 +0200

    fixed

--- js/common.js
+++ js/common.js
@@ -14,6 +14,40 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
+	
+
+	//slick
+	$('.js-slick').slick({
+		infinite: true,
+		slidesToShow: 3,
+		arrows: true,
+		slidesToScroll: 3,
+		responsive: [
+			{
+		        breakpoint: 1900,
+		        settings: {
+		        	slidesToShow: 5,
+		        	slidesToScroll: 5,
+		    	}
+		    },
+			{
+		        breakpoint: 1600,
+		        settings: {
+		        	slidesToShow: 3,
+		        	slidesToScroll: 3,
+		    	}
+		    },
+			{
+		        breakpoint: 768,
+		        settings: {
+		        	slidesToShow: 2,
+		        	slidesToScroll: 2,
+		    	}
+		    },
+		]
+	});
+
+	//menu
 
 	$('.js-btn-menu').on('click', function() {
 		$('.js-menu').toggleClass('is-active');
@@ -27,5 +61,95 @@ head.ready(function() {
 		});
 	});
 
+	//galleries synchronize (ITEM)
+	var slideshows = $('.cycle-slideshow').on('cycle-next cycle-prev', function(e, opts) {
+	  slideshows.not(this).cycle('goto', opts.currSlide);
+	});
+
+	$('.gallery__pager .slide').click(function() {
+	  var index = $('.gallery__pager').data('cycle.API').getSlideIndex(this);
+	  console.log(index);
+	  slideshows.cycle('goto', index);
+	});
+
+	//select
+	$(document).ready(function() {
+	    $(document).click(function() {
+	        $(".js-select-list").hide();
+	        $(".js-select").removeClass("is-active");
+	    });
+	    function selectList() {
+	        var select = $(".js-select");
+	        var select_list = $(".js-select-list");
+	        $("body").on("click", ".js-select", function(event){
+	            if ($(this).hasClass("is-active")) {
+	                select.removeClass("is-active");
+	                select_list.hide();
+	            }
+	            else {
+	                select.removeClass("is-active");
+	                select_list.hide();
+	                $(this).find(".js-select-list").show();
+	                $(this).addClass("is-active");
+	            }
+	            event.stopPropagation();
+	        });
+	        $("body").on("click", ".js-select-list li", function(event){
+	            var id = $(this).attr("data-id");
+	            var text = $(this).text();
+	            $(this).parents(".js-select").find(".js-select-text").text(text);
+	            $(this).parents(".js-select").find(".js-select-input").val(id);
+	            $(this).parent().hide();
+	            $(this).parents(".js-select").removeClass("is-active");
+	            event.stopPropagation();
+	        });
+	    }  
+	    
+	    selectList();
+	    $("body").on("click", ".js-select", function(event){
+	        event.stopPropagation();
+	    });
+	    
+	});
+	
+	//colors
+	$(document).ready(function() {
+	    $(document).click(function() {
+	        $(".js-colors-list").hide();
+	        $(".js-colors").removeClass("is-active");
+	    });
+	    function selectList() {
+	        var colors = $(".js-colors");
+	        var colors_list = $(".js-colors-list");
+	        $("body").on("click", ".js-colors", function(event){
+	            if ($(this).hasClass("is-active")) {
+	                colors.removeClass("is-active");
+	                colors_list.hide();
+	            }
+	            else {
+	                colors.removeClass("is-active");
+	                colors_list.hide();
+	                $(this).find(".js-colors-list").show();
+	                $(this).addClass("is-active");
+	            }
+	            event.stopPropagation();
+	        });
+	        $("body").on("click", ".js-colors-list li", function(event){
+	            var id = $(this).attr("data-id");
+	            var text = $(this).text();
+	            $(this).parents(".js-colors").find(".js-colors-text").html($(this).html());
+	            $(this).parents(".js-colors").find(".js-colors-input").val(id);
+	            $(this).parent().hide();
+	            $(this).parents(".js-colors").removeClass("is-active");
+	            event.stopPropagation();
+	        });
+	    }  
+	    
+	    selectList();
+	    $("body").on("click", ".js-colors", function(event){
+	        event.stopPropagation();
+	    });
+	    
+	});
 
 });
--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/cycle.carousel.js",
 					"js/lib/cycle-swipe.js",
 					"js/lib/jquery-ui.min.js",
+					"js/lib/slick.min.js",
 					"js/common.js");
